882a2b0bb68ac7035b717f9b205f0e6975746593
Brandon Liu <bdon@bdon.org>
2021-04-10 21:07:14 +0800

    add compat/json_style

---
+++ src/compat/json_style.ts
@@ -0,0 +1,141 @@
+import { FillSymbolizer, LineSymbolizer, LineLabelSymbolizer, TextSymbolizer, exp } from './../symbolizer'
+
+export function filterFn(arr) {
+    // hack around "$type"
+    if (arr.includes("$type")) {
+        return f => true 
+    } else if (arr[0] == "==") {
+        return f => f[arr[1]] === arr[2]
+    } else if (arr[0] == "!=") {
+        return f => f[arr[1]] !== arr[2]
+    } else if (arr[0] === "<") {
+        return f => f[arr[1]] < arr[2]
+    } else if (arr[0] === ">") {
+        return f => f[arr[1]] > arr[2]
+    } else if (arr[0] === "in") {
+        return f => arr.slice(2,arr.length).includes(f[arr[1]])
+    } else if (arr[0] === "!in") {
+        return f => !arr.slice(2,arr.length).includes(f[arr[1]])
+    } else if (arr[0] === "has") {
+        return f => f.hasOwnProperty(arr[1])
+    } else if (arr[0] === "all") {
+        let parts = arr.slice(1,arr.length).map(e => filterFn(e))
+        return f => parts.every(p => { return p(f)})
+    } else if (arr[0] === "any") {
+        let parts = arr.slice(1,arr.length).map(e => filterFn(e))
+        return f => parts.some(p => { return p(f) })
+    } else {
+        console.log("Unimplemented: ",arr[0])
+    }
+}
+
+export function numberFn(obj) {
+    if (!obj) return obj
+    if (typeof obj == "number") {
+        return obj
+    } 
+    if (obj.base && obj.stops) {
+        return z => exp(z,obj.base,obj.stops)
+    } else {
+        console.log("Unimplemented: ", obj)
+    }
+}
+
+export function getFont(obj,mapping = {}) {
+    let fontfaces = []
+    for (let wanted_face of obj['text-font']) {
+        if (mapping.hasOwnProperty(wanted_face)) {
+            fontfaces.push(mapping[wanted_face])
+        }
+    }
+    if (fontfaces.length === 0) fontfaces.push('sans-serif')
+
+    let text_size = obj['text-size']
+    if (typeof text_size == 'number') {
+        return `${text_size}px ${fontfaces.join(', ')}`
+    } else if (text_size.stops) {
+        text_size.base ||= 1.4
+        return z => {
+            let t = numberFn(text_size)
+            return `${t(z)}px ${fontfaces.join(', ')}`
+        }
+    } else {
+        console.log("Can't parse font: ", obj)
+    }
+}
+
+export function json_style(obj) {
+    let paint_style = []
+    let label_style = []
+    let refs = new Map<string,any>()
+
+    for (var layer of obj.layers) {
+        refs.set(layer.id,layer)
+
+        if (layer.layout && layer.layout.visibility == "none") {
+            continue
+        }
+
+        if (layer.ref) {
+           let referenced = refs.get(layer.ref)
+           layer.type = referenced.type
+           layer.filter = referenced.filter
+           layer.source = referenced['source']
+           layer['source-layer'] = referenced['source-layer']
+        }
+
+        let sourceLayer = layer['source-layer']
+        var symbolizer
+
+        var filter = undefined
+        if (layer.filter) {
+            filter = filterFn(layer.filter)
+        }
+
+        // ignore background-color?
+        if (layer.type == "fill") {
+            paint_style.push({
+                dataLayer: layer['source-layer'],
+                filter:filter,
+                symbolizer: new FillSymbolizer({
+                    fill:layer.paint['fill-color'],
+                    opacity:layer.paint['fill-opacity']
+                })
+            })
+        } else if (layer.type == "line") {
+            paint_style.push({
+                dataLayer: layer['source-layer'],
+                filter:filter,
+                symbolizer: new LineSymbolizer({
+                    color:layer.paint['line-color'],
+                    width:numberFn(layer.paint["line-width"])
+                })
+            })
+        } else if (layer.type == "symbol") {
+            if (layer.layout["symbol-placement"] == "line") {
+                label_style.push({
+                    dataLayer: layer['source-layer'],
+                    filter:filter,
+                    symbolizer: new LineLabelSymbolizer({
+                        fill:layer.paint['text-color'],
+                        width:layer.paint['text-halo-width'],
+                        stroke:layer.paint['text-halo-color']
+                    })
+                })
+            } else {
+                label_style.push({
+                    dataLayer: layer['source-layer'],
+                    filter:filter,
+                    symbolizer: new TextSymbolizer({
+                        font: getFont(layer.layout),
+                        fill: layer.paint['text-color'],
+                        stroke: layer.paint['text-halo-color'],
+                        width:layer.paint['text-halo-width']
+                    })
+                })
+            }
+        }
+    }
+
+    return {paint_style:paint_style,label_style:label_style}
+}
---
+++ test/json_style.test.ts
@@ -0,0 +1,84 @@
+import { filterFn, numberFn, getFont } from '../src/compat/json_style'
+
+test("==",() => {
+    let f = filterFn(['==','building','yes'])
+    expect(f({"building":"yes"}))
+})
+test("!=",() => {
+    let f = filterFn(['!=','building','yes'])
+    expect(!f({"building":"yes"}))
+    expect(f({"building":"no"}))
+})
+test("<",() => {
+    let f = filterFn(['<','level',3])
+    expect(f({"level":2}))
+    expect(!f({"level":3}))
+})
+test(">",() => {
+    let f = filterFn(['>','level',3])
+    expect(f({"level":4}))
+    expect(!f({"level":3}))
+})
+test("in",() => {
+    let f = filterFn(['in','type','foo','bar'])
+    expect(f({"type":"foo"}))
+    expect(f({"type":"bar"}))
+    expect(!f({"type":"baz"}))
+})
+test("!in",() => {
+    let f = filterFn(['in','type','foo','bar'])
+    expect(!f({"type":"bar"}))
+    expect(f({"type":"baz"}))
+})
+test("has",() => {
+    let f = filterFn(['has','type'])
+    expect(f({"type":"foo"}))
+    expect(!f({}))
+})
+test("all",() => {
+    let f = filterFn(['all',["==","building","yes"],["==","type","foo"]])
+    expect(!f({"building":"yes"}))
+    expect!(f({"type":"foo"}))
+    expect(f({"building":"yes","type":"foo"}))
+})
+test("any",() => {
+    let f = filterFn(['any',["==","building","yes"],["==","type","foo"]])
+    expect(!f({}))
+    expect(f({"building":"yes"}))
+    expect(f({"type":"foo"}))
+    expect(f({"building":"yes","type":"foo"}))
+})
+
+test("numberFn constant", () => {
+    let n = numberFn(5)
+    expect(n).toEqual(5)
+    n = numberFn(undefined)
+    expect(n).toEqual(undefined)
+})
+
+test("numberFn function", () => {
+    let n = numberFn({base:1,stops:[[14,0],[16,2]]})
+    expect(n.length).toEqual(1)
+    expect(n(14)).toEqual(0)
+    expect(n(15)).toEqual(1)
+    expect(n(16)).toEqual(2)
+})
+test("font", () => {
+   let n = getFont({'text-font':['Noto'],'text-size':14}) 
+   expect(n).toEqual('14px sans-serif')
+
+   n = getFont({'text-font':['Noto'],'text-size':15}) 
+   expect(n).toEqual('15px sans-serif')
+
+   n = getFont({'text-font':['Noto'],'text-size':15},{'Noto':'serif'}) 
+   expect(n).toEqual('15px serif')
+
+   n = getFont({'text-font':['Boto','Noto'],'text-size':15},{'Noto':'serif','Boto':'Comic Sans'}) 
+   expect(n).toEqual('15px Comic Sans, serif')
+})
+test("font size fn", () => {
+   let n = getFont({'text-font':['Noto'],'text-size':{'base':1,'stops':[[14,1],[16,3]]}}) 
+   expect(n(14)).toEqual('1px sans-serif')
+   expect(n(15)).toEqual('2px sans-serif')
+   expect(n(16)).toEqual('3px sans-serif')
+})
