5c467bbd73f246693fc250fc0794d1a4aad37ba4
Brandon Liu <bdon@bdon.org>
2021-06-26 00:10:52 +0800

    test exit code for CI

--- test/index.ts
+++ test/index.ts
@@ -5,11 +5,13 @@ import text from './text.test'
 import tilecache from './tilecache.test'
 import view from './view.test'
 
-!(async function() {
-  await json_style.run()
-  await line.run()
-  await symbolizer.run()
-  await text.run()
-  await tilecache.run()
-  await view.run()
+(async function() {
+    let r = []
+    r.push(await json_style.run())
+    r.push(await line.run())
+    r.push(await symbolizer.run())
+    r.push(await text.run())
+    r.push(await tilecache.run())
+    r.push(await view.run())
+    if (process.env.CI && !r.every(Boolean)) process.exit(1)
 })()
