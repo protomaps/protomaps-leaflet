12991821a32e7c0cc20e6f78e46cc970b9f1b9c0
Brandon Liu <bdon@bdon.org>
2021-07-02 23:10:50 +0800

    color tweaks

--- src/default_style/light.ts
+++ src/default_style/light.ts
@@ -3,7 +3,7 @@ import { paintRules, labelRules, DefaultStyleParams } from './style'
 const light:DefaultStyleParams = {
     earth:"#FFFBF6",
     glacier:"#ffffff",
-    residential:"#e7e7e7",
+    residential:"#F4F4F8",
     hospital:"#FFF6F6",
     cemetery:"#EFF2EE",
     school:"#F7F6FF",
@@ -26,11 +26,11 @@ const light:DefaultStyleParams = {
     mask:"#dddddd",
     countryLabel:"#aaaaaa",
     cityLabel:"#6C6C6C",
-    stateLabel:"#aaaaaa",
+    stateLabel:"#999999",
     neighbourhoodLabel:"#888888",
     landuseLabel:"#898989",
-    waterLabel:"#717171",
-    naturalLabel:"4c4c4c",
+    waterLabel:"#41ABDC",
+    naturalLabel:"#4B8F14",
     roadsLabel:"#888888",
     poisLabel:"#606060"
 }
