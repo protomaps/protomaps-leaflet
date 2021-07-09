05e754a5e0c9b87c4c4f5195369699c8d6823c02
Brandon Liu <bdon@bdon.org>
2021-07-09 17:46:37 +0800

    clean up module toplevel

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import { Static } from './frontends/static'
-import { LeafletLayer, leafletLayer } from './frontends/leaflet'
-import { createPattern, GroupSymbolizer, CircleSymbolizer, PolygonSymbolizer, IconSymbolizer, LineSymbolizer, CenteredTextSymbolizer, OffsetTextSymbolizer, PolygonLabelSymbolizer, LineLabelSymbolizer, arr, exp } from './symbolizer'
-import { json_style } from './compat/json_style'
-import { Font, Sprites } from './task'
-import { PMTiles } from 'pmtiles'
-
-export { createPattern, PMTiles, LeafletLayer, leafletLayer, Static, Font, Sprites, GroupSymbolizer, CircleSymbolizer, PolygonSymbolizer, LineSymbolizer, CenteredTextSymbolizer, OffsetTextSymbolizer, PolygonLabelSymbolizer, LineLabelSymbolizer, IconSymbolizer, json_style, arr, exp }
+export * from './frontends/static'
+export * from './frontends/leaflet'
+export * from './symbolizer'
+export * from './task'
+export * from './default_style/light'
+export * from './compat/json_style'
+export { PMTiles } from 'pmtiles'
