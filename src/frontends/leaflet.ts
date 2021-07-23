98adbd394d7b0a9f4eb4c70e3cf9b25bf5e31d81
Matt Mills <matt@felt.com>
2021-07-23 15:12:36 -0700

    add the option to increase max zoom

--- src/frontends/leaflet.ts
+++ src/frontends/leaflet.ts
@@ -68,9 +68,14 @@ const leafletLayer = (options:any):any => {
                 source = new ZxySource(options.url)
             }
 
+            let maxZoom = 14;
+            if (options.maxZoom) {
+              maxZoom = options.maxZoom;
+            }
+
             this.tasks = options.tasks || []
             let cache = new TileCache(source,1024)
-            this.view = new View(cache,14,2)
+            this.view = new View(cache,maxZoom,2)
             this.debug = options.debug
             let scratch = document.createElement('canvas').getContext('2d')
             this.scratch = scratch
