098bbc871f14051bb6b7ee56d827b2bed1b24771
Brandon Liu <bdon@bdon.org>
2021-10-08 15:34:42 +0800

    add types for linelabel, remove angles

--- src/line.ts
+++ src/line.ts
@@ -1,14 +1,32 @@
 // @ts-ignore
 import Point from "@mapbox/point-geometry";
 
+export interface LabelableSegment {
+  length: number;
+  beginIndex: number;
+  beginDistance: number;
+  endIndex: number;
+  endDistance: number;
+}
+
 // code from https://github.com/naturalatlas/linelabel (Apache2)
-const linelabel = (pts, max_angle_delta) => {
+const linelabel = (
+  pts: Point[],
+  max_angle_delta: number
+): LabelableSegment[] => {
   var chunks = [];
-  var cur_angles = [null];
-  var a, b, c, i = 0, n = 0, d = 0;
-  var abmag = 0, bcmag = 0;
-  var abx = 0, aby = 0;
-  var bcx = 0, bcy = 0;
+  var a,
+    b,
+    c,
+    i = 0,
+    n = 0,
+    d = 0;
+  var abmag = 0,
+    bcmag = 0;
+  var abx = 0,
+    aby = 0;
+  var bcx = 0,
+    bcy = 0;
   var dt = 0;
   var i_start = 0;
   var d_start = 0;
@@ -16,20 +34,23 @@ const linelabel = (pts, max_angle_delta) => {
   if (pts.length < 2) return [];
   if (pts.length === 2) {
     d = Math.sqrt(
-      Math.pow(pts[1].x - pts[0].x, 2) +
-      Math.pow(pts[1].y - pts[0].y, 2));
+      Math.pow(pts[1].x - pts[0].x, 2) + Math.pow(pts[1].y - pts[0].y, 2)
+    );
 
-    return [{
-      length: d,
-      beginIndex: 0,
-      beginDistance: 0,
-      endIndex: 2,
-      endDistance: d,
-      angles: [null, null]
-    }];
+    return [
+      {
+        length: d,
+        beginIndex: 0,
+        beginDistance: 0,
+        endIndex: 2,
+        endDistance: d,
+      },
+    ];
   }
 
-  abmag = Math.sqrt(Math.pow(pts[1].x - pts[0].x, 2) + Math.pow(pts[1].y - pts[0].y, 2));
+  abmag = Math.sqrt(
+    Math.pow(pts[1].x - pts[0].x, 2) + Math.pow(pts[1].y - pts[0].y, 2)
+  );
   for (i = 1, n = pts.length - 1; i < n; i++) {
     a = pts[i - 1];
     b = pts[i];
@@ -42,7 +63,6 @@ const linelabel = (pts, max_angle_delta) => {
     d += abmag;
 
     dt = Math.acos((abx * bcx + aby * bcy) / (abmag * bcmag));
-    cur_angles.push(dt);
     if (dt > max_angle_delta) {
       chunks.push({
         length: d - d_start,
@@ -50,16 +70,13 @@ const linelabel = (pts, max_angle_delta) => {
         beginIndex: i_start,
         endIndex: i + 1,
         endDistance: d,
-        angles: cur_angles
       });
       i_start = i;
       d_start = d;
-      cur_angles = [dt];
     }
     abmag = bcmag;
   }
 
-  cur_angles.push(null);
   if (i - i_start > 0) {
     chunks.push({
       length: d - d_start + bcmag,
@@ -67,11 +84,10 @@ const linelabel = (pts, max_angle_delta) => {
       beginDistance: d_start,
       endIndex: i + 1,
       endDistance: d + bcmag,
-      angles: cur_angles
     });
   }
   return chunks;
-}
+};
 
 export function simpleLabel(mls: any, minimum: number) {
   let longestStart;
