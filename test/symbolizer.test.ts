5e45ee334f672884abce483d500d0e5bd8d2668b
Isaac Besora <isaac.besora@gmail.com>
2021-10-18 12:48:18 +0200

    Adds cubic bezier interpolation method

--- test/symbolizer.test.ts
+++ test/symbolizer.test.ts
@@ -1,4 +1,4 @@
-import { exp, step, linear } from "../src/symbolizer";
+import { exp, step, linear, cubicBezier } from "../src/symbolizer";
 import assert from "assert";
 import baretest from "baretest";
 
@@ -40,11 +40,18 @@ test("step", async () => {
   assert.equal(5, f(0));
   assert.equal(10, f(4));
   assert.equal(10, f(5));
-  f = step(5, [[4, 10], [5, 15]]);
+  f = step(5, [
+    [4, 10],
+    [5, 15],
+  ]);
   assert.equal(5, f(0));
   assert.equal(10, f(4));
   assert.equal(15, f(5));
-  f = step(5, [[4, 10], [5, 12], [6, 15]]);
+  f = step(5, [
+    [4, 10],
+    [5, 12],
+    [6, 15],
+  ]);
   assert.equal(5, f(0));
   assert.equal(10, f(4));
   assert.equal(12, f(5));
@@ -67,6 +74,29 @@ test("linear", async () => {
   assert.equal(15, f(5));
   assert.equal(20, f(6));
   assert.equal(20, f(7));
+});
+
+const precisionMatch = 0.0001;
+const almostEqual = (a, b) => Math.abs(a - b) <= precisionMatch;
+
+test("cubic-bezier", async () => {
+  let f = cubicBezier(0.42, 0, 0.58, 1, []);
+  assert.equal(0, f(0));
+  f = cubicBezier(0.42, 0, 0.58, 1, [
+    [0, 0],
+    [100, 100],
+  ]);
+  assert(almostEqual(0, f(0)));
+  assert(almostEqual(1.97224, f(10)));
+  assert(almostEqual(8.16597, f(20)));
+  assert(almostEqual(18.7395, f(30)));
+  assert(almostEqual(33.1883, f(40)));
+  assert(almostEqual(50, f(50)));
+  assert(almostEqual(66.8116, f(60)));
+  assert(almostEqual(81.2604, f(70)));
+  assert(almostEqual(91.834, f(80)));
+  assert(almostEqual(98.0277, f(90)));
+  assert(almostEqual(100, f(100)));
 });
 
 export default test;
